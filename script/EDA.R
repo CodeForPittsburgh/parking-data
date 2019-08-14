92d46f4ee44cb2156742b2e7227f03ffa4abf554
Conor Tompkins <conorotompkins@gmail.com>
2019-08-14 23:50:18 +0000

    added code to explore dataset, initial mapping

--- script/EDA.R
+++ script/EDA.R
@@ -1,5 +1,12 @@
 library(tidyverse)
-df <- read_csv("strip2018.csv")
+library(lubridate)
+library(gganimate)
+
+theme_set(theme_bw())
+
+df <- read_csv("strip2018.csv") %>% 
+  mutate(hour = hour(payment_start_utc),
+         wday = wday(payment_start_utc, label = TRUE))
 df_geo <- read_csv("https://data.wprdc.org/datastore/dump/db139ccd-6753-48ad-b3ff-118fe2223d55")
 
 df %>% 
@@ -10,6 +17,23 @@ glimpse(df)
 df %>% 
   count(zone, sort = TRUE)
 
-df %>% 
+df <- df %>% 
   left_join(df_geo, by = c("meter_id" = "id"))
 
+df_trend <- df %>% 
+  #filter(!str_detect(location_type, "Virtual")) %>% 
+  count(location_type, meter_id, wday, hour) %>% 
+  arrange(desc(n))
+
+df_trend %>% 
+  count(location_type, sort = TRUE)
+
+df_trend %>% 
+  ggplot(aes(hour, n)) +
+  geom_jitter() +
+  facet_wrap(~location_type, scales = "free_y", ncol = 1)
+
+df %>% 
+  count(meter_id, longitude, latitude) %>% 
+  ggplot(aes(longitude, latitude, size = n)) +
+  geom_point()
