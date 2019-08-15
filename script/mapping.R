42ec78d366d7e8547128f369795fc4ad1f6ef8d6
Conor Tompkins <conorotompkins@gmail.com>
2019-08-15 00:01:13 +0000

    copied and pasted test code

--- script/mapping.R
+++ script/mapping.R
@@ -2,3 +2,28 @@
 
 #OSM
 #https://dominicroye.github.io/en/2018/accessing-openstreetmap-data-with-r/
+
+library(tidyverse)
+library(osmdata)
+library(sf)
+library(ggmap)
+
+q <- getbb("Pittsburgh")%>%
+  opq()%>%
+  add_osm_feature("amenity", "cinema")
+
+cinema <- osmdata_sf(q)
+cinema
+
+pgh_map <- get_map(getbb("Pittsburgh"),maptype = "toner-background")
+
+#final map
+ggmap(pgh_map)+
+  geom_sf(data=cinema$osm_points,
+          inherit.aes =FALSE,
+          colour="#238443",
+          fill="#004529",
+          alpha=.5,
+          size=4,
+          shape=21)+
+  labs(x="",y="")
